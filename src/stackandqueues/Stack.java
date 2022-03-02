96025fa6952c2dc8c5085be73ded3475d3a2a969
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-02 21:14:43 +0530

    created a stack to peek qand pop still stack gets empty file

--- src/stackandqueues/Stack.java
+++ src/stackandqueues/Stack.java
@@ -2,7 +2,7 @@ package stackandqueues;
 
 public class Stack {
 
-	int stack[] = new int[3];
+	int stack[] = new int[3]; // taking Stack to store 3 value
 	int top = 0;
 
 	public void push(int data) {
@@ -10,10 +10,42 @@ public class Stack {
 		top++;
 	}
 
+	public int peek() { // peeking top values of stack
+		int data = 0;
+		if (isEmpty()) {
+			System.out.println("The Stack is empty");
+		} else {
+			data = stack[top - 1];
+		}
+		return data;
+	}
+
+	public int pop() { // deleting top value
+		int data = 0;
+		if (isEmpty()) {
+			System.out.println("The Stack is empty");
+		} else {
+			top--;
+			data = stack[top];
+			stack[top] = 0;
+		}
+		return data;
+	}
+
+	public boolean isEmpty() { // defined empty<=0
+
+		return top <= 0;
+	}
+
+	public int size() { // to check stack size
+		System.out.println("\n" + top);
+		return top;
+	}
+
 	public void show() // method to print every element
 	{
 		for (int n : stack) {
-			System.out.print(n + " ");
+			System.out.print(n + "->");
 		}
 	}
 
@@ -23,5 +55,16 @@ public class Stack {
 		nums.push(30);
 		nums.push(70);
 		nums.show();
+
+		nums.size();// check the stack size
+
+		// to peek and pop the value alternatly till the stack is empty
+		for (int i = 0; i <= 3; i++) {
+			System.out.println("peeking value" + nums.peek());
+			nums.show();
+			System.out.println("pop value" + nums.pop());
+			nums.show();
+		}
+
 	}
 }
