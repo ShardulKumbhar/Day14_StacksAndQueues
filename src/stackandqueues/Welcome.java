7cee92e8b35c8fc17ac8dc214eeaea4ea3bda1a5
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-03-03 23:41:33 +0530

    added welcome message

---
+++ src/stackandqueues/Welcome.java
@@ -0,0 +1,12 @@
+package stackandqueues;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Stacks And Queues Problem");
+		System.out.println("Solved by Array");
+
+
+	}
+
+}
